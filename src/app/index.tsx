2de5a7ce5b6526dd3840c9145b06b27535ea2e59
Mihail Mojsoski <mihail-mojsoski@outlook.com>
2022-04-08 00:49:40 +0200

    Started working on the web terminal and aded fantasque sans mono font

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,3 +1,61 @@
-import React from "react";
+import React from "react"
+import {MayHaveChildren, withStyle, withStyleOverride} from "./base";
 
-export default () => <span>Hello, World</span>;
+const Console = withStyle({
+    background: 'black',
+    color: 'white',
+    fontFamily: 'Fantasque Sans Mono',
+    height: '100vh',
+    width: '100vw',
+    display: 'block',
+    padding: '0',
+    margin: '0',
+    whiteSpace: 'pre-wrap'
+})
+
+type ConsoleTextProps = {color? : string}
+type ConsoleText = ConsoleTextProps&MayHaveChildren | string
+const Text = withStyleOverride<ConsoleTextProps>({
+    display: 'inline',
+    padding: '0',
+    margin: '0',
+    whiteSpace: 'inherit'
+}, ["color"])
+
+function withConsoleColor(color : string): (children: string) => ConsoleText {
+    return (children : string) => { return { color, children } }
+}
+const red = withConsoleColor('red')
+const yellow = withConsoleColor('yellow')
+const blue = withConsoleColor('blue')
+
+const art = "\n" +
+    "      ___     \n" +
+    "     /__/\\    \n" +
+    "    |  |::\\   \n" +
+    "    |  |:|:\\  \n" +
+    "  __|__|:|\\:\\ \n" +
+    " /__/::::| \\:\\\n" +
+    " \\  \\:\\~~\\__\\/\n" +
+    "  \\  \\:\\      \n" +
+    "   \\  \\:\\     \n" +
+    "    \\  \\:\\    \n" +
+    "     \\__\\/    \n"
+
+const content = [
+    art, '\n',
+    "I am currently working on a ", yellow("Web Terminal"), ".\n",
+    "It's still ", red("work in progress"), "!\n\n",
+    "Currently it can render text and ", blue('colors'), '.'
+]
+
+function asConsoleContent(block: ConsoleText) {
+    if (typeof block === "string") {
+        block = { children: block }
+    }
+    return <Text {...block}/>
+}
+
+export default () => <Console>
+    {content.map(asConsoleContent)}
+</Console>
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,9 @@
 import "normalize.css";
 import "./css/styles.css";
+import './fonts/FantasqueSansMono-Bold-decl.css'
+import './fonts/FantasqueSansMono-Italic-decl.css'
+import './fonts/FantasqueSansMono-Regular-decl.css'
+import './fonts/FantasqueSansMono-BoldItalic-decl.css'
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app";
